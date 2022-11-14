c07b1df9d5a55dd84301dea2f1c47b318a7c2948
swelanauguste <swelanauguste@gmail.com>
2022-11-14 18:28:19 -0400

    update add_faqs

--- app/home/management/commands/add_faqs.py
+++ app/home/management/commands/add_faqs.py
@@ -11,10 +11,10 @@ class Command(BaseCommand):
         with open(f"static/csv/faqs.csv", "r") as file:
             reader = csv.reader(file)
             for i, row in enumerate(reader):
-                title = row[0].lower().replace("\n", "")
+                question = row[0].lower().replace("\n", "")
                 details = row[1].lower().replace("\n", "")
                 FAQ.objects.get_or_create(
-                    title=title,
+                    question=question,
                     details=details,
                 )
-                self.stdout.write(self.style.SUCCESS(f"{title} added"))
+                self.stdout.write(self.style.SUCCESS(f"{question} added"))
