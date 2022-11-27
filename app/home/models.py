7b08ad11dcd44eee8053c0d1c0263471579ecc94
swelanauguste <swelanauguste@gmail.com>
2022-11-27 08:03:29 -0400

    added sentence case

--- app/home/models.py
+++ app/home/models.py
@@ -5,9 +5,9 @@ class WhatWeDo(models.Model):
     title = models.CharField(max_length=50, unique=True)
     details = models.TextField()
     updated = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
-        verbose_name_plural = 'what we do'
+        verbose_name_plural = "what we do"
 
     def __str__(self):
         return self.title
@@ -26,9 +26,26 @@ class MythAndFact(models.Model):
     title = models.CharField(max_length=50, unique=True)
     details = models.TextField()
     updated = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
-        verbose_name_plural = 'myths and facts'
+        verbose_name_plural = "myths and facts"
+
+    def get_sentence_case(self):
+        output = ""
+        isFirstWord = True
+
+        for c in self.details:
+            if isFirstWord and not c.isspace():
+                c = c.upper()
+                isFirstWord = False
+            elif not isFirstWord and c in ".!?":
+                isFirstWord = True
+            else:
+                c = c.lower()
+
+            output = output + c
+
+        return output
 
     def __str__(self):
         return self.title
