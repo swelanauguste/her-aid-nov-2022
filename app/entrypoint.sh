d7e6a0a48d9efc4c791a6f572220e0a12caaa403
swelanauguste <swelanauguste@gmail.com>
2023-02-02 16:04:28 -0400

    docker file

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
-python manage.py flush --no-input
+# if [ "$DATABASE" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
+
+#     while ! nc -z $SQL_HOST $SQL_PORT; do
+#       sleep 0.1
+#     done
+
+#     echo "PostgreSQL started"
+# fi
+
+# python manage.py flush --no-input
 
 python manage.py migrate
-python manage.py createsuperuser --username king --email no-reply@mail.com --no-input
+# python manage.py createsuperuser --username king --email no-reply@mail.com --no-input
 
-python manage.py add_districts
-python manage.py add_locations
-python manage.py add_referral_categories
-python manage.py add_referrals
+# python manage.py add_districts
+# python manage.py add_locations
+# python manage.py add_referral_categories
+# python manage.py add_referrals
 
-python manage.py add_faqs
-python manage.py add_myths_and_facts
+# python manage.py add_faqs
+# python manage.py add_myths_and_facts
 
-python manage.py add_screening_questions
-python manage.py add_types_of_abuse
+# python manage.py add_screening_questions
+# python manage.py add_types_of_abuse
 
-python manage.py add_rights
+# python manage.py add_rights
 
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 
 
 exec "$@"
