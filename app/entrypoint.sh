b7cc0b0fde984b4e44de7136d464e16ac8f2e2ad
swelanauguste <swelanauguste@gmail.com>
2023-03-13 05:56:17 +0000

    git

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -14,22 +14,22 @@
 # python manage.py flush --no-input
 
 python manage.py migrate
-# python manage.py createsuperuser --username king --email no-reply@mail.com --no-input
+#python manage.py createsuperuser --username king --email no-reply@mail.com --no-input
 
-# python manage.py add_districts
-# python manage.py add_locations
-# python manage.py add_referral_categories
-# python manage.py add_referrals
+#python manage.py add_districts
+#python manage.py add_locations
+#python manage.py add_referral_categories
+#python manage.py add_referrals
 
-# python manage.py add_faqs
-# python manage.py add_myths_and_facts
+#python manage.py add_faqs
+#python manage.py add_myths_and_facts
 
-# python manage.py add_screening_questions
-# python manage.py add_types_of_abuse
+#python manage.py add_screening_questions
+#python manage.py add_types_of_abuse
 
-# python manage.py add_rights
+#python manage.py add_rights
 
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 
 exec "$@"
